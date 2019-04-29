b2a4e2bd041f5e6a9b2572e5937ed0d84b809fce
Mitchell Berger <mitchberger106@gmail.com>
2019-04-29 18:23:06 -0400

    Added muscle group

--- Ece563/app/src/main/java/berger/mitchell/ece563/Sources/AvailableWorkoutSource.java
+++ Ece563/app/src/main/java/berger/mitchell/ece563/Sources/AvailableWorkoutSource.java
@@ -2,15 +2,21 @@ package berger.mitchell.ece563.Sources;
 
 public class AvailableWorkoutSource {
     private String name;
+    private String bodyType;
 
     public AvailableWorkoutSource() {
     }
 
-    public AvailableWorkoutSource(String name) {
+    public AvailableWorkoutSource(String name, String bodyType) {
         this.name = name;
+        this.bodyType = bodyType;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getBodyType() {
+        return bodyType;
     }
 }
